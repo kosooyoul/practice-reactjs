3c5019549c39f1baa6bfbfadd8fd19b6bf9d2901
kosooyoul <kosooyoul@gmail.com>
2023-03-12 18:01:49 +0900

    Upt. rpg, chara set

--- rpg/src/game/CharaSet.js
+++ rpg/src/game/CharaSet.js
@@ -1,24 +1,28 @@
-const makeCharaSet = (x, y, step, sw, sh) => {
+const makeCharaSet = (x, y, sw, sh) => {
   const charaSet = { left: [], right: [], up: [], down: [] };
-  for (var i = 0; i < step; i++) {
-    charaSet.up.push({ x: i * sw, y: 0 * sh, width: sw, height: sh });
-  }
-  for (var i = 0; i < step; i++) {
-    charaSet.right.push({ x: i * sw, y: 1 * sh, width: sw, height: sh });
-  }
-  for (var i = 0; i < step; i++) {
-    charaSet.down.push({ x: i * sw, y: 2 * sh, width: sw, height: sh });
-  }
-  for (var i = 0; i < step; i++) {
-    charaSet.left.push({ x: i * sw, y: 3 * sh, width: sw, height: sh });
-  }
+  charaSet.up.push({ x: 0 * sw, y: 0 * sh, width: sw, height: sh });
+  charaSet.up.push({ x: 1 * sw, y: 0 * sh, width: sw, height: sh });
+  charaSet.up.push({ x: 2 * sw, y: 0 * sh, width: sw, height: sh });
+  charaSet.up.push({ x: 1 * sw, y: 0 * sh, width: sw, height: sh });
+  charaSet.right.push({ x: 0 * sw, y: 1 * sh, width: sw, height: sh });
+  charaSet.right.push({ x: 1 * sw, y: 1 * sh, width: sw, height: sh });
+  charaSet.right.push({ x: 2 * sw, y: 1 * sh, width: sw, height: sh });
+  charaSet.right.push({ x: 1 * sw, y: 1 * sh, width: sw, height: sh });
+  charaSet.down.push({ x: 0 * sw, y: 2 * sh, width: sw, height: sh });
+  charaSet.down.push({ x: 1 * sw, y: 2 * sh, width: sw, height: sh });
+  charaSet.down.push({ x: 2 * sw, y: 2 * sh, width: sw, height: sh });
+  charaSet.down.push({ x: 1 * sw, y: 2 * sh, width: sw, height: sh });
+  charaSet.left.push({ x: 0 * sw, y: 3 * sh, width: sw, height: sh });
+  charaSet.left.push({ x: 1 * sw, y: 3 * sh, width: sw, height: sh });
+  charaSet.left.push({ x: 2 * sw, y: 3 * sh, width: sw, height: sh });
+  charaSet.left.push({ x: 1 * sw, y: 3 * sh, width: sw, height: sh });
   return [charaSet];
 }
 
 const CHARA_SET_IMAGE_URL = 'http://localhost:3000/assets/chara.bmp';
 const CHARA_SIZE_WIDTH = 24;
 const CHARA_SIZE_HEIGHT = 32;
-const CHARA_SET = makeCharaSet(0, 0, 4, CHARA_SIZE_WIDTH, CHARA_SIZE_HEIGHT);
+const CHARA_SET = makeCharaSet(0, 0, CHARA_SIZE_WIDTH, CHARA_SIZE_HEIGHT);
 
 export default class CharaSet {
   context;
